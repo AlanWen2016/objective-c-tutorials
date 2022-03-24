66bbb43e334a53cd8e205dcc088e6eac22b203e7
alan_wen <alan_wen@kingdee.com>
2022-03-24 16:58:54 +0800

    feat: add Hello boject

--- Hello Jobs/ViewController.m
+++ Hello Jobs/ViewController.m
@@ -16,6 +16,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.view addSubview:({
+        UILabel *label = [[UILabel alloc] init];
+        label.text = @"Hello Objective-C";
+        [label sizeToFit];
+        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+        label;
+    })];
 }
 
 
